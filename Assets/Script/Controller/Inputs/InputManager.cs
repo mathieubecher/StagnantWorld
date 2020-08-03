91c4e436f51d85f385b01ce9a87751ced8981a5f
Becher <mbelkhenchir@gmail.com>
2020-08-04 01:47:55 +0200

    Character controller & animator & player input

---
+++ Assets/Script/Controller/Inputs/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public Vector3 move;
+    
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move");
+        Vector2 direction = context.ReadValue<Vector2>();
+        move = new Vector3(direction.x, 0, direction.y);
+    }
+}
---
+++ Assets/Script/Controller/State/IddleState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IddleState : AbstractState
+{
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _rigidbody.velocity = _controller.inputs.move;
+    }
+    
+}
